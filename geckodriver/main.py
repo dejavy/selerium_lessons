546c881a716d11a1ae07c9477d1a87e8e066dead
dejavy <dejavydejavy@gmail.com>
2022-03-09 17:16:05 +0200

    Add test Firefox

---
+++ geckodriver/main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+url = "https://www.olx.ua/"
+driver = webdriver.Firefox(executable_path="/Users/dejavy/PycharmProjects/selerium_lessons/geckodriver/geckodriver")
+
+try:
+    driver.get(url=url)
+    time.sleep(5)
+    # driver.get(url="https://www.stackoverflow.com/")
+    # time.sleep(5)
+    # driver.refresh()
+
+    # driver.get_screenshot_as_file("1.png")
+    # driver.get(url="https://www.stackoverflow.com/")
+    # time.sleep(5)
+    # driver.save_screenshot("2.png")
+    # time.sleep(5)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
+
+
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
