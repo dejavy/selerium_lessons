ec7a3c32e7bfdde61710e56bb332949569652f61
dejavy <dejavydejavy@gmail.com>
2022-03-10 17:43:01 +0200

    add 'useragent and random_useragent'

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,20 +1,48 @@
 from selenium import webdriver
 import time
-url = "https://www.instagram.com/"
-driver = webdriver.Chrome(executable_path="/Users/dejavy/PycharmProjects/selerium_lessons/chromedriver/chromedriver")
+import random
+from fake_useragent import UserAgent
+
+# urls
+url_1 = "https://www.instagram.com/"
+url_2 = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
+
+user_agents_list = [
+    "HelloWorld",
+    "best_of_the_best",
+    "python_today"
+]
+
+useragent = UserAgent()
+
+# options
+# https://deviceatlas.com/blog/list-of-user-agent-strings
+# https://deviceatlas.com/blog/mobile-browser-user-agent-strings
+
+options = webdriver.ChromeOptions()
+# options.add_argument("user-agent=HelloWorld:)")
+# options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36")
+# options.add_argument(f"user-agent={random.choice(user_agents_list)}")
+# options.add_argument(f"user-agent={useragent.opera}")
+options.add_argument(f"user-agent={useragent.random}")
+# driver
+driver = webdriver.Chrome(
+    executable_path="/Users/dejavy/PycharmProjects/selerium_lessons/chromedriver/chromedriver",
+    options=options
+)
 
 try:
-    driver.get(url=url)
-    time.sleep(5)
+    driver.get(url=url_2)
+    time.sleep(3)
     # driver.get(url="https://www.stackoverflow.com/")
     # time.sleep(5)
     # driver.refresh()
 
-    driver.get_screenshot_as_file("1.png")
-    driver.get(url="https://www.stackoverflow.com/")
-    time.sleep(5)
-    driver.save_screenshot("2.png")
-    time.sleep(5)
+    # driver.get_screenshot_as_file("1.png")
+    # driver.get(url="https://www.stackoverflow.com/")
+    # time.sleep(5)
+    # driver.save_screenshot("2.png")
+    # time.sleep(5)
 except Exception as ex:
     print(ex)
 finally:
--- geckodriver/main.py
+++ geckodriver/main.py
@@ -1,11 +1,27 @@
 from selenium import webdriver
 import time
-url = "https://www.olx.ua/"
-driver = webdriver.Firefox(executable_path="/Users/dejavy/PycharmProjects/selerium_lessons/geckodriver/geckodriver")
+import  random
+from fake_useragent import UserAgent
+
+# urls
+url_1 = "https://www.olx.ua/"
+url_2 = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
+# add useragent
+useragent = UserAgent()
+# optoins
+options = webdriver.FirefoxOptions()
+# change useragent
+# options.set_preference("general.useragent.override", "Hello friend")
+options.set_preference("general.useragent.override", useragent.random)
+# driwer
+driver = webdriver.Firefox(
+    executable_path="/Users/dejavy/PycharmProjects/selerium_lessons/geckodriver/geckodriver",
+    options=options
+)
 
 try:
-    driver.get(url=url)
-    time.sleep(5)
+    driver.get(url=url_2)
+    time.sleep(3)
     # driver.get(url="https://www.stackoverflow.com/")
     # time.sleep(5)
     # driver.refresh()
